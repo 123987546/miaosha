772a6b0ebcd7a7856bf23b1e54b8adaba1cad34f
廖泽宇 <630843901@qq.com>
2020-02-04 22:33:09 +0800

    update1

--- src/main/java/com/miaosha/day1/controller/GoodsController.java
+++ src/main/java/com/miaosha/day1/controller/GoodsController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletResponse;
@@ -35,6 +36,30 @@ public class GoodsController {
         return "goods_list";
     }
 
+    @RequestMapping("/to_detail/{goodsId}")
+    public String list(Model model,MiaoshaUser user, @PathVariable("goodsId")long goodsId) {
+        model.addAttribute("user", user);
+        GoodsVo goodsVo=goodsService.getGoodsVoByGoodsId(goodsId);
+        model.addAttribute("goods",goodsVo);
+        long startTime=goodsVo.getStartDate().getTime();
+        long endTime=goodsVo.getEndDate().getTime();
+        long now=System.currentTimeMillis();
+        int miaoshaStatus=0;
+        int remianSecond=0;
+        if(startTime>now){//未开始
+            miaoshaStatus=0;
+            remianSecond=(int)((startTime-now)/1000);
+        }else if(endTime<now){//已结束
+            miaoshaStatus=2;
+            remianSecond=-1;
+        }else{//正在
+            miaoshaStatus=1;
+            remianSecond=0;
+        }
+        model.addAttribute("miaoshaStatus",miaoshaStatus);
+        model.addAttribute("remainSecond",remianSecond);
+        return "goods_list";
+    }
 
 
 }
