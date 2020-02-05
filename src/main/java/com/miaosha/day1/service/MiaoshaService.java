13d70ae3e632e1e10969a23d5be57273d42561ef
廖泽宇 <630843901@qq.com>
2020-02-05 18:01:49 +0800

    update2

---
+++ src/main/java/com/miaosha/day1/service/MiaoshaService.java
@@ -0,0 +1,27 @@
+package com.miaosha.day1.service;
+
+import com.miaosha.day1.domain.MiaoshaUser;
+import com.miaosha.day1.domain.OrderInfo;
+import com.miaosha.day1.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class MiaoshaService {
+
+    @Autowired
+    GoodsService goodsService;
+
+    @Autowired
+    OrderService orderService;
+
+    @Transactional
+    public OrderInfo miaosha(MiaoshaUser user, GoodsVo goods) {
+        //减库存 下订单 写入秒杀订单
+        goodsService.reduceStock(goods);
+        //order_info maiosha_order
+        return orderService.createOrder(user, goods);
+    }
+
+}
